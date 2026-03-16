8bc33b3959c3f26fbf075baf01d1c0b1b955b4bb
Mithra <mithra29.venu@gmail.com>
2026-03-16 14:16:47 -0400

    Updated controllers

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -2,7 +2,7 @@ import Product from "../models/product.js";
 
 export async function createProduct(req, res) {
   try {
-    const newProduct = Product.create(req.body);
+    const newProduct = await Product.create(req.body);
     console.log(newProduct);
     res.status(201).json(newProduct);
   } catch (error) {
@@ -13,11 +13,11 @@ export async function createProduct(req, res) {
 export async function getProduct(req, res) {
   try {
     const id = req.params.id;
-    const product = Product.findById(id);
+    const product = await Product.findById(id);
     console.log(product);
     res.status(200).json(product);
   } catch (error) {
-    res.status(404).json({message:`product with ${id} is not found`});
+    res.status(404).json({ message: `product with ${id} is not found` });
   }
 }
 
@@ -25,42 +25,52 @@ export async function updateProduct(req, res) {
   try {
     const id = req.params.id;
     const options = { new: true };
-    const updatedProduct = Product.findByIdAndUpdate(id, req.body, options);
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id,
+      req.body,
+      options,
+    );
     console.log(updatedProduct);
     res.status(201).json(updatedProduct);
   } catch (error) {
-    res.status(404).json({message:`product with ${id} is not found`});
-
+    res.status(404).json({ message: `product with ${id} is not found` });
   }
 }
 
-
-export async function deleteProduct(req,res) {
-      try {
+export async function deleteProduct(req, res) {
+  try {
     const id = req.params.id;
-    const deletedProduct = Product.findByIdAndDelete(id);
-    if(!deletedProduct){
-    res.status(404).json({message:`product with ${id} is not found`});
-    }else{
-    console.log(deletedProduct);
-    res.status(201).json(deletedProduct);
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    if (!deletedProduct) {
+      res.status(404).json({ message: `product with ${id} is not found` });
+    } else {
+      console.log(deletedProduct);
+      res.status(201).json(deletedProduct);
     }
   } catch (error) {
     res.status(400).json({ message: error.message });
-
   }
 }
 
-
 export async function getProducts(req, res) {
   try {
-    const {category, minPrice, maxPrice, sortBy} = req.query;
+    const { category, minPrice, maxPrice, sortBy } = req.query;
     const page = req.query.page || 1;
     const limit = req.query.limit || 10;
-    const products = Product.find({});
+    const products = await Product.find({
+      $or: [
+        { category: category },
+        { price: {$gte: minPrice}  },
+        { price: {$lte: maxPrice} },
+      ],
+    })
+      .sort({ price: -1 })
+      .skip((page - 1) * limit)
+      .limit(limit);
+
     console.log(products);
     res.status(200).json(products);
   } catch (error) {
-    res.status(404).json({message:`product with ${id} is not found`});
+    res.status(404).json({ message: `product with ${id} is not found` });
   }
 }
